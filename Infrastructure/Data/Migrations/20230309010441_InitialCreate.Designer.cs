745c10bbaf63b4a54ec38c08ef02cae275f3ab50
tahomaspice <124075982+tahomaspice@users.noreply.github.com>
2023-03-09 10:47:47 -0800

    End of Sec 5

--- Infrastructure/Data/Migrations/20230309010441_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20230309010441_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20230308012947_InitialCreate")]
+    [Migration("20230309010441_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
